57a8cd0844f011342c28776a1406cc36039d0567
ayoub <ayoubaarab571@gmail.com>
2024-04-05 23:46:46 +0000

    Update

--- src/pages/Client/pages/home/Home.jsx
+++ src/pages/Client/pages/home/Home.jsx
@@ -1,23 +1,27 @@
-import React, { useEffect } from "react";
-import HomeSlider from "./slideer/Slider";
-import "./home.css";
-import Banner from "../../components/banner/Banner";
-import Header from "pages/Client/components/header/Header";
-import Footer from "pages/Client/components/footer/Footer";
+import React, { useEffect } from "react"
+import HomeSlider from "./slideer/Slider"
+import "./home.css"
+import Banner from "../../components/banner/Banner"
+import Header from "pages/Client/components/header/Header"
+import Footer from "pages/Client/components/footer/Footer"
 
-const Home = (props) => {
-
+const Home = props => {
   useEffect(() => {
-    document.title = "Home";
-  }, []);
+    document.title = "Home"
+  }, [])
   return (
     <>
-      <Header categories={props.categories} subcategories={props.subcategories} cartitems={props.cartitems} fetchCartItems={props.fetchCartItems}/>
+      <Header
+        categories={props.categories}
+        subcategories={props.subcategories}
+        cartitems={props.cartitems}
+        fetchCartItems={props.fetchCartItems}
+      />
       <HomeSlider />
       <Banner />
       <Footer />
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
