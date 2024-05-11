ac4624890638d60b04415074b6f2b8983bb9f60d
ayoub <ayoubaarab571@gmail.com>
2024-05-11 21:23:48 +0100

    advanced routes

--- src/pages/Products/product/index.js
+++ src/pages/Products/product/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { MDBDataTable } from "mdbreact"
 import {
   Row,
@@ -17,9 +17,12 @@ import Breadcrumbs from "../../../components/Common/Breadcrumb"
 import "../../../../src/assets/scss/datatables.scss"
 import ProductTable from "./table"
 import AddForm from "./addForm"
+import { post, get, del, put } from "helpers/api_helper";
 
 const Productindex = () => {
   const [modal_add, setmodal_add] = useState(false)
+  const [products, setProducts] = useState([])
+  const [error, setError] = useState("")
 
   const removeBodyCss = () => {
     document.body.classList.add("no_padding")
@@ -30,7 +33,67 @@ const Productindex = () => {
     removeBodyCss()
   }
 
-  document.title = "Products Table | Veltrix - React Admin & Dashboard Template"
+  const handleSave = async (values) => {
+    try {
+      const response = await post(
+        "http://127.0.0.1:8000/api/products",
+        values
+      )
+      if (response.status === 200) {
+        setmodal_add(false)
+      } else {
+        setError("")
+        setmodal_add(false)
+        fetchProducts()
+      }
+      console.log(data);
+    } catch (error) {
+      setError(error.response.data.message)
+    }
+  }
+  const handleEdit = async (id, values) => {
+    try {
+      const response = await put(`http://127.0.0.1:8000/api/products/${id}`, {
+        ...values,
+        id: id,
+      })
+      if (response.status === 200) {
+      } else {
+        setError("")
+        fetchProducts()
+      }
+    } catch (error) {
+      console.error("Error editing products:", error)
+      setError(error.response.data.message)
+    }
+  }
+  
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await get("http://127.0.0.1:8000/api/products");
+
+      const data = await response.data;
+      setProducts(data);
+    } catch (error) {
+      setError(error.response.data.message);
+
+    }
+  };
+  const handleDelete = async product => {
+    try {
+      const response = await del(
+        `http://127.0.0.1:8000/api/products/${product.id}`
+      )
+      fetchProducts()
+    } catch (error) {
+      setError(error.response.data.message)
+    }
+  }
+  document.title = "Products Table"
   return (
     <React.Fragment>
       <Col sm={6} md={4} xl={3}>
@@ -38,7 +101,9 @@ const Productindex = () => {
           <ModalHeader className="mt-0" toggle={tog_add}>
             Add New Product
           </ModalHeader>
-          <ModalBody> <AddForm isOpen={modal_add} toggle={tog_add} /> </ModalBody>
+          <ModalBody>
+            <AddForm handleCancel={tog_add} handleSave={handleSave} error={error}  />
+          </ModalBody>
         </Modal>
       </Col>
       <div className="page-content">
@@ -53,7 +118,7 @@ const Productindex = () => {
             <Col className="col-12">
               <Card>
                 <CardBody>
-                  <ProductTable />
+                  <ProductTable products={products} handleDelete={handleDelete} handleEdit={handleEdit}/>
                 </CardBody>
               </Card>
             </Col>
--- src/pages/Products/product/table.js
+++ src/pages/Products/product/table.js
@@ -2,97 +2,89 @@ import React, { useState, useEffect } from "react"
 import { MDBDataTable } from "mdbreact"
 import { Modal, ModalBody, ModalHeader, Button } from "reactstrap"
 import EditForm from "./editForm"
-import DeleteModal from "./DeleteModal"
-import { get } from "helpers/api_helper"
+import { Row, Col, Badge } from "reactstrap";
 
-const ProductTable = () => {
+const ProductTable = (props) => {
   const [modal_edit, setmodal_edit] = useState(false)
   const [modal_delete, setmodal_delete] = useState(false)
   const [products, setProducts] = useState([])
   const [selectedProduct, setSelectedProduct] = useState(null)
-  const [Error, setError] = useState(null)
-  
-  
-  const fetchProducts = async () => {
-    try {
-      const response = await get("http://127.0.0.1:8000/api/products");
-  
-      const data = await response.data;
-      setProducts(data);
-    } catch (error) {
-      setError(error.response.data.message);
-  
-    }
-  };
 
-  useEffect(() => {
-   fetchProducts()
+  // useEffect(() => {
+  //   fetch("http://127.0.0.1:8000/api/products")
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       setProducts(data.data)
+  //     })
+  //     .catch(error => {
+  //       console.error("Error fetching products:", error)
+  //     })
+  //   }, [])
+    
+  //   const handleEditSubmit = updatedProduct => {
+  //     const updatedProducts = products.map(product =>
+  //       product.id === updatedProduct.id ? updatedProduct : product
+  //     );
+  //     setProducts(updatedProducts);
+  
+  //     fetch(`http://127.0.0.1:8000/api/products/${updatedProduct.id}`, {
+  //       method: "PUT",
+  //       headers: {
+  //         "Content-Type": "application/json"
+  //       },
+  //       body: JSON.stringify(updatedProduct)
+  //     })
+  //       .then(response => {
+  //         if (!response.ok) {
+  //           console.error("Failed to update product");
+  //         }
+  //       })
+  //       .catch(error => {
+  //         console.error("Error updating product:", error);
+  //       });
+  //   };
+    
+  //   const handleDelete = () => {
+  //     if (!selectedProduct) return
+  
+  //     fetch(`http://127.0.0.1:8000/api/products/${selectedProduct.id}`, {
+  //       method: "DELETE",
+  //     })
+  //       .then(response => {
+  //         if (response.ok) {
+  //           // Remove the deleted product from the products list
+  //           const updatedProducts = products.filter(
+  //             product => product.id !== selectedProduct.id
+  //           )
+  //           setProducts(updatedProducts)
+  //           // Close the delete modal
+  //           setmodal_delete(false)
+  //         } else {
+  //           console.error("Failed to delete product")
+  //         }
+  //       })
+  //       .catch(error => {
+  //         console.error("Error deleting product:", error)
+  //       })
+  //   }
 
-  },[]);
 
-
-  const removeBodyCss = () => {
+    const removeBodyCss = () => {
     document.body.classList.add("no_padding")
   }
 
-  const tog_edit = product => {
+  const tog_edit = (product) => {
+    setmodal_edit(!modal_edit);
     setSelectedProduct(product)
-    setmodal_edit(!modal_edit)
-    removeBodyCss()
-  }
-
-  const tog_delete = productId => {
-    console.log("Deleting product with ID:", productId)
-    setmodal_delete(!modal_delete)
-    removeBodyCss()
-  }
-
-  const handleEditSubmit = updatedProduct => {
-    const updatedProducts = products.map(product =>
-      product.id === updatedProduct.id ? updatedProduct : product
-    );
-    setProducts(updatedProducts);
-
-    fetch(`http://127.0.0.1:8000/api/products/${updatedProduct.id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(updatedProduct)
-    })
-      .then(response => {
-        if (!response.ok) {
-          console.error("Failed to update product");
-        }
-      })
-      .catch(error => {
-        console.error("Error updating product:", error);
-      });
+    removeBodyCss();
+  };
+  const tog_delete = (product) => {
+    setmodal_delete(!modal_delete);
+    setSelectedProduct(product)
+    removeBodyCss();
   };
   
-  const handleDelete = () => {
-    if (!selectedProduct) return
 
-    fetch(`http://127.0.0.1:8000/api/products/${selectedProduct.id}`, {
-      method: "DELETE",
-    })
-      .then(response => {
-        if (response.ok) {
-          // Remove the deleted product from the products list
-          const updatedProducts = products.filter(
-            product => product.id !== selectedProduct.id
-          )
-          setProducts(updatedProducts)
-          // Close the delete modal
-          setmodal_delete(false)
-        } else {
-          console.error("Failed to delete product")
-        }
-      })
-      .catch(error => {
-        console.error("Error deleting product:", error)
-      })
-  }
   const data = {
     columns: [
       {
@@ -143,7 +135,7 @@ const ProductTable = () => {
         width: 150,
       },
     ],
-    rows: products.map(product => ({
+    rows: props.products.map((product) => ({
       id: product.id,
       price_unit: product.price_unit,
       price_total: product.price_total,
@@ -155,20 +147,15 @@ const ProductTable = () => {
         <div className="flex">
           <button
             className="btn btn-info btn-sm mx-2"
-            onClick={() =>{
-              setSelectedProduct(product)
-              tog_edit(product)}
-            } 
+            onClick={() => tog_edit(product)}
+
             
           >
             <i className="ti-pencil-alt "></i>{" "}
           </button>
           <button
             className="btn btn-danger btn-sm"
-            onClick={() => {
-              setSelectedProduct(product)
-              tog_delete()
-            }}
+            onClick={() => tog_delete(product)}
           >
             <i className="ti-trash"></i>
           </button>
@@ -187,21 +174,51 @@ const ProductTable = () => {
               <EditForm
                 isOpen={modal_edit}
                 product={selectedProduct}
-                onSubmit={handleEditSubmit}
                 toggle={tog_edit}
-              />
+                handleEdit={(id, values) => {
+                  props.handleEdit(id, values)
+                  setmodal_edit(false)
+                }} handleCancel={tog_edit} />
             </ModalBody>
           </Modal>
           <Modal isOpen={modal_delete} toggle={tog_delete} centered>
-            <ModalHeader className="mt-0" toggle={tog_delete}>
-              Delete Product
-            </ModalHeader>
+            <ModalHeader className="mt-0" toggle={tog_delete}>Delete Product</ModalHeader>
             <ModalBody>
-              <DeleteModal
-                isOpen={modal_delete}
-                toggle={tog_delete}
-                onDelete={handleDelete}
-              />
+              <Row>
+                <Col lg={12}>
+                  <div className="text-center">
+                    <i
+                      className="mdi mdi-alert-circle-outline"
+                      style={{ fontSize: "9em", color: "orange" }}
+                    />
+                    <h2>Are you sure?</h2>
+                    <h4>{"You won't be able to revert this!"}</h4>
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <div className="text-center mt-3">
+                    <button
+                      type="button"
+                      className="btn btn-success btn-lg ms-2"
+                      onClick={() => {
+                        props.handleDelete(selectedProduct)
+                        tog_delete()
+                      }}
+                    >
+                      Yes, delete it!
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-danger btn-lg ms-2"
+                      onClick={tog_delete}
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </Col>
+              </Row>
             </ModalBody>
           </Modal>
         <MDBDataTable responsive bordered data={data} />
