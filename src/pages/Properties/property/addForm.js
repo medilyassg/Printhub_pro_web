72f3ff820d64defb1b7065756a81d7881c5c6f55
ayoub <ayoubaarab571@gmail.com>
2024-05-12 02:46:40 +0100

    update add and edit forms

--- src/pages/Properties/property/addForm.js
+++ src/pages/Properties/property/addForm.js
@@ -1,119 +1,118 @@
+import { useFormik } from "formik";
 import React, { useState, useEffect } from "react";
+import { Row, Col, CardBody, Card, Form, FormFeedback, Label, Input } from "reactstrap";
+import Select from "react-select";
+import * as Yup from "yup";
+import { get, post } from "helpers/api_helper";
 
-const AddForm = ({ isOpen, toggle }) => {
-  const [newProperty, setNewProperty] = useState({
-    nom: "",
-    description: "",
-    price: "",
-    sub_categories: [],
-  });
-
-  const [subCategories, setSubCategories] = useState([]);
+const AddForm = props => {
+  const [selectedMulti, setselectedMulti] = useState(null);
+  const [selectedSubcategoriesIDs, setselectedSubcategoriesIDs] = useState([]);
+  const [subcategories, setSubcategories] = useState([]);
 
   useEffect(() => {
-    fetch("http://127.0.0.1:8000/api/subcategories")
-      .then(response => response.json())
-      .then(data => {
-        setSubCategories(data.data);
-      })
-      .catch(error => {
-        console.error("Error fetching subcategories:", error);
-      });
+    fetchSubcategories();
   }, []);
 
-  const handleChange = e => {
-    const { name, value } = e.target;
-    setNewProperty({ ...newProperty, [name]: value });
-  };
+  const fetchSubcategories = async () => {
+    try {
+      const response = await get("http://127.0.0.1:8000/api/subcategories");
 
-  const handleSubcategoryChange = e => {
-    const { options } = e.target;
-    const selectedSubcategories = [];
-    for (let i = 0; i < options.length; i++) {
-      if (options[i].selected) {
-        selectedSubcategories.push(parseInt(options[i].value));
-      }
+      const data = await response.data;
+      setSubcategories(data);
+    } catch (error) {
+      // setError(error.response.data.message);
+
     }
-    setNewProperty({ ...newProperty, sub_categories: selectedSubcategories });
+  };
+  const optionGroup = subcategories.map((subcategory) => ({
+    label: subcategory.nom,
+    value: subcategory.id
+  }))
+
+
+  const handleMulti = (selectedOptions) => {
+    const selectedIds = selectedOptions.map(option => option.value);
+    setselectedSubcategoriesIDs(selectedIds)
+    setselectedMulti(selectedOptions);
   };
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    fetch("http://127.0.0.1:8000/api/properties", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(newProperty)
-    })
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("Failed to add new property");
-        }
-      })
-      .then(data => {
-        console.log("New property added successfully:", data);
-        // Close the modal after submitting
-        toggle();
-      })
-      .catch(error => {
-        console.error("Error adding new property:", error);
-      });
-  };
+  const validationSchema = Yup.object({
+    nom: Yup.string().required("Please Enter a name"),
+    description: Yup.string().required("Please Enter description"),
+    price: Yup.number().required("Please Enter price"),
+  });
+
+
+  const validation = useFormik({
+    initialValues : {
+      nom: "",
+      description: "",
+      price: "",
+    },
+    validationSchema,
+    onSubmit: (values) =>{
+      props.handleSave({ ...values, sub_categories: selectedSubcategoriesIDs })},
+  });
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={validation.handleSubmit}>
       <div className="mb-3">
-        <label className="form-label">Name</label>
-        <input
+        <Label className="form-label">Name</Label>
+        <Input
           type="text"
           className="form-control"
           name="nom"
-          value={newProperty.nom}
-          onChange={handleChange}
+          onChange={validation.handleChange}
+          onBlur={validation.handleBlur}
+          value={validation.values.nom || ""}
+          invalid={validation.touched.nom && validation.errors.nom}
         />
+        <FormFeedback>{validation.errors.nom}</FormFeedback>
       </div>
       <div className="mb-3">
-        <label className="form-label">Description</label>
-        <input
+        <Label className="form-label">Description</Label>
+        <Input
           type="text"
           className="form-control"
           name="description"
-          value={newProperty.description}
-          onChange={handleChange}
+          id="description"
+          onChange={validation.handleChange}
+          onBlur={validation.handleBlur}
+          value={validation.values.description || ""}
+          invalid={validation.touched.description && validation.errors.description}
         />
+        <FormFeedback>{validation.errors.description}</FormFeedback>
       </div>
       <div className="mb-3">
-        <label className="form-label">Price</label>
-        <input
+        <Label className="form-label">Price</Label>
+        <Input
           type="number"
           step={0.1}
           className="form-control"
           name="price"
-          value={newProperty.price}
-          onChange={handleChange}
+          id="price"
+          onChange={validation.handleChange}
+          onBlur={validation.handleBlur}
+          value={validation.values.price || ""}
+          invalid={validation.touched.price && validation.errors.price}
         />
+        <FormFeedback>{validation.errors.price}</FormFeedback>
       </div>
       <div className="mb-3">
-        <label className="form-label">Subcategories</label>
-        <select
-          className="form-select"
+        <Label className="form-label">Subcategories</Label>
+        <Select
           name="sub_categories"
-          multiple
-          value={newProperty.sub_categories}
-          onChange={handleSubcategoryChange}
-        >
-          {subCategories.map(subCategory => (
-            <option key={subCategory.id} value={subCategory.id}>
-              {subCategory.nom}
-            </option>
-          ))}
-        </select>
+          value={selectedMulti}
+          isMulti={true}
+          onChange={handleMulti}
+          options={optionGroup}
+          classNamePrefix="select2-selection"
+        />
       </div>
+      <FormFeedback>{validation.errors.subcategories}</FormFeedback>
       <button type="submit" className="btn btn-primary">Add Property</button>
-      <button type="button" className="btn btn-secondary ms-2" onClick={toggle}>Cancel</button>
+      <button type="button" className="btn btn-secondary ms-2" onClick={props.handleCancel}>Cancel</button>
     </form>
   )
 }
--- src/pages/Properties/property/editForm.js
+++ src/pages/Properties/property/editForm.js
@@ -1,85 +1,125 @@
-import React, { useState } from "react";
+import { useFormik } from "formik";
+import React, { useState, useEffect } from "react";
+import { Label, Input, FormFeedback } from "reactstrap";
+import Select from "react-select";
+import * as Yup from "yup";
+import { get } from "helpers/api_helper";
 
-const EditForm = ({ property, onSubmit, toggle, subCategories }) => {
-  const [editedProperty, setEditedProperty] = useState(property);
+const EditForm = (props) => {
+  const [selectedMulti, setselectedMulti] = useState([]);
+  const [selectedSubcategoriesIDs, setselectedSubcategoriesIDs] = useState([]);
+  const [subcategories, setSubcategories] = useState([]);
 
-  const handleChange = e => {
-    const { name, value } = e.target;
-    setEditedProperty({ ...editedProperty, [name]: value });
-  };
+  useEffect(() => {
+    fetchSubcategories();
+  }, []);
 
-  const handleSubcategoryChange = e => {
-    const { options } = e.target;
-    const selectedSubcategories = [];
-    for (let i = 0; i < options.length; i++) {
-      if (options[i].selected) {
-        selectedSubcategories.push(parseInt(options[i].value));
-      }
+  useEffect(() => {
+    if (props.property.sub_categories) {
+      const selectedProperty = props.property.sub_categories.map((subcategory) => ({
+        label: subcategory.nom,
+        value: subcategory.id
+      }));
+      setselectedMulti(selectedProperty);
+      setselectedSubcategoriesIDs(selectedProperty.map((subcategory) => subcategory.value));
     }
-    setEditedProperty({ ...editedProperty, sub_categories: selectedSubcategories });
+  }, [props.property]);
+
+  const fetchSubcategories = async () => {
+    try {
+      const response = await get("http://127.0.0.1:8000/api/subcategories");
+      const data = await response.data;
+      setSubcategories(data);
+    } catch (error) {
+      console.error("Error fetching subcategories:", error);
+    }
   };
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    onSubmit(editedProperty);
-    toggle();
+  const optionGroup = subcategories.map((subcategory) => ({
+    label: subcategory.nom,
+    value: subcategory.id
+  }));
+
+  const handleMulti = (selectedOptions) => {
+    const selectedIds = selectedOptions.map((option) => option.value);
+    setselectedSubcategoriesIDs(selectedIds);
+    setselectedMulti(selectedOptions);
   };
+
+  const validationSchema = Yup.object({
+    nom: Yup.string().required("Please Enter a name"),
+    description: Yup.string().required("Please Enter description"),
+    price: Yup.number().required("Please Enter price"),
+  });
+
+  const validation = useFormik({
+    initialValues: {
+      nom: props.property.nom || "",
+      description: props.property.description || "",
+      price: props.property.price || "",
+    },
+    validationSchema,
+    onSubmit: (values) => {
+      props.handleEdit(props.property.id, { ...values, sub_categories: selectedSubcategoriesIDs });
+    },
+  });
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={validation.handleSubmit}>
       <div className="mb-3">
-        <label className="form-label">Name</label>
-        <input
+        <Label className="form-label">Name</Label>
+        <Input
           type="text"
           className="form-control"
           name="nom"
-          value={editedProperty.nom}
-          onChange={handleChange}
+          onChange={validation.handleChange}
+          onBlur={validation.handleBlur}
+          value={validation.values.nom}
+          invalid={validation.touched.nom && validation.errors.nom}
         />
+        <FormFeedback>{validation.errors.nom}</FormFeedback>
       </div>
       <div className="mb-3">
-        <label className="form-label">Description</label>
-        <input
+        <Label className="form-label">Description</Label>
+        <Input
           type="text"
           className="form-control"
           name="description"
-          value={editedProperty.description}
-          onChange={handleChange}
+          onChange={validation.handleChange}
+          onBlur={validation.handleBlur}
+          value={validation.values.description}
+          invalid={validation.touched.description && validation.errors.description}
         />
+        <FormFeedback>{validation.errors.description}</FormFeedback>
       </div>
       <div className="mb-3">
-        <label className="form-label">Price</label>
-        <input
+        <Label className="form-label">Price</Label>
+        <Input
           type="number"
           step={0.1}
           className="form-control"
           name="price"
-          value={editedProperty.price}
-          onChange={handleChange}
+          onChange={validation.handleChange}
+          onBlur={validation.handleBlur}
+          value={validation.values.price}
+          invalid={validation.touched.price && validation.errors.price}
         />
+        <FormFeedback>{validation.errors.price}</FormFeedback>
       </div>
       <div className="mb-3">
-        <label className="form-label">Subcategories</label>
-        <select
-          className="form-select"
+        <Label className="form-label">Subcategories</Label>
+        <Select
           name="sub_categories"
-          multiple
-          value={editedProperty.sub_categories}
-          onChange={handleSubcategoryChange}
-        >
-          {subCategories.map(subCategory => (
-            <option key={subCategory.id} value={subCategory.id}>
-              {subCategory.nom}
-            </option>
-          ))}
-        </select>
+          value={selectedMulti}
+          isMulti={true}
+          onChange={handleMulti}
+          options={optionGroup}
+          classNamePrefix="select2-selection"
+        />
       </div>
-      <button type="submit" className="btn btn-primary">
-        Save Changes
-      </button>
-      <button type="button" className="btn btn-secondary" onClick={toggle}>
-        Cancel
-      </button>
+      <FormFeedback>{validation.errors.sub_categories}</FormFeedback>
+      <button type="submit" className="btn btn-primary">Update Property</button>
+      <button type="button" className="btn btn-secondary ms-2" onClick={props.handleCancel}>Cancel</button>
     </form>
   );
 };
